68517aa2d7ab12fa829d734088578d7b5fbfdbd3
jahallb1 <62314714+jahallb1@users.noreply.github.com>
2021-02-17 18:23:22 -0500

    working on index pages for each router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div>
-      <h1>Here we go!</h1>
-    </div>
+    <Router>
+      <div>
+        <Route excat path="/" />
+        <Route exact path="/login" componet={Login} />
+      </div>
+    </Router>
   );
 }
 
